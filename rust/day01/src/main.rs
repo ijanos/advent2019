81cb742f4d5b33125efaf5b2efabd197f91ac2f2
János Illés <ijanos@gmail.com>
2019-12-01 08:21:44 +0100

    rust day01

---
+++ rust/day01/src/main.rs
@@ -0,0 +1,28 @@
+use std::io;
+use std::io::prelude::*;
+
+fn allfuel(mass: isize) -> isize {
+    let mut all = 0;
+    let mut mass = mass;
+    loop {
+        let additionalfuel = mass / 3 - 2;
+        if additionalfuel > 0 {
+            all += additionalfuel
+        } else {
+            break;
+        }
+        mass = additionalfuel;
+    }
+    all
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let modules = stdin.lock().lines().map(|line| line.unwrap().parse::<isize>().unwrap()).collect::<Vec<_>>();
+
+    let part1: isize = modules.iter().map(|m| m / 3 - 2 ).sum();
+    println!("Part 1: {}", part1);
+
+    let part2: isize = modules.iter().map(|&m| allfuel(m)).sum();
+    println!("Part 2: {}", part2);
+}
