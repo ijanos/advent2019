5dd7f74d28812c0e6b96c670c9fd79b0b482125b
János Illés <ijanos@gmail.com>
2019-12-05 23:20:39 +0100

    a more DRY rust day05

--- rust/day05/src/main.rs
+++ rust/day05/src/main.rs
@@ -4,230 +4,97 @@ use std::io::prelude::*;
 #[derive(Debug)]
 enum Instruction {
     Add(isize, isize, usize),
-    Mul(isize, isize, usize),
-    Inp(usize),
-    Out(isize),
+    Eq(isize, isize, usize),
     Hlt,
-    Jtr(isize, usize),
-    Jfl(isize, usize),
+    Inp(usize),
+    Jnz(isize, usize),
+    Jz(isize, usize),
     Lt(isize, isize, usize),
-    Eq(isize, isize, usize)
+    Mul(isize, isize, usize),
+    Out(isize)
 }
 
 fn opcode_decode(pc: usize, memory: &Vec<isize>) -> Instruction {
-    match memory[pc] {
+    let resolve_a = |a| if memory[pc] / 100 % 10 == 1 { a } else { memory[a as usize] };
+    let resolve_b = |b| if memory[pc] / 1000 % 10 == 1 { b } else { memory[b as usize] };
+    match memory[pc] % 100 {
         1 => {
-            let a = memory[pc + 1] as usize;
-            let b = memory[pc + 2] as usize;
+            let a = memory[pc + 1];
+            let b = memory[pc + 2];
             let out = memory[pc + 3] as usize;
-            Instruction::Add(memory[a], memory[b], out)
+            Instruction::Add(resolve_a(a), resolve_b(b), out)
         }
         2 => {
-            let a = memory[pc + 1] as usize;
-            let b = memory[pc + 2] as usize;
+            let a = memory[pc + 1];
+            let b = memory[pc + 2];
             let out = memory[pc + 3] as usize;
-            Instruction::Mul(memory[a], memory[b], out)
+            Instruction::Mul(resolve_a(a), resolve_b(b), out)
         }
         3 => {
             let a = memory[pc + 1] as usize;
             Instruction::Inp(a)
         }
         4 => {
-            let a = memory[pc + 1] as usize;
-            Instruction::Out(memory[a])
-        }
-        99 => Instruction::Hlt,
-        1102 => {
             let a = memory[pc + 1];
-            let b = memory[pc + 2];
-            let out = memory[pc + 3] as usize;
-            Instruction::Mul(a, b, out)
-        }
-        104 => {
-            let a = memory[pc + 1];
-            Instruction::Out(a)
-        }
-        101 => {
-            let a = memory[pc + 1];
-            let b = memory[pc + 2] as usize;
-            let out = memory[pc + 3] as usize;
-            Instruction::Add(a, memory[b], out)
-        }
-        102 => {
-            let a = memory[pc + 1];
-            let b = memory[pc + 2] as usize;
-            let out = memory[pc + 3] as usize;
-            Instruction::Mul(a, memory[b], out)
-        }
-        1001 => {
-            let a = memory[pc + 1] as usize;
-            let b = memory[pc + 2];
-            let out = memory[pc + 3] as usize;
-            Instruction::Add(memory[a], b, out)
-        }
-        1002 => {
-            let a = memory[pc + 1] as usize;
-            let b = memory[pc + 2];
-            let out = memory[pc + 3] as usize;
-            Instruction::Mul(memory[a], b, out)
-        }
-        1101 => {
-            let a = memory[pc + 1];
-            let b = memory[pc + 2];
-            let out = memory[pc + 3] as usize;
-            Instruction::Add(a, b, out)
+            Instruction::Out(resolve_a(a))
         }
         5 => {
-            let a = memory[pc + 1] as usize;
-            let b = memory[pc + 2] as usize;
-            Instruction::Jtr(memory[a], memory[b] as usize)
-        }
-        1105 => {
             let a = memory[pc + 1];
-            let b = memory[pc + 2] as usize;
-            Instruction::Jtr(a, b)
+            let b = memory[pc + 2];
+            Instruction::Jnz(resolve_a(a), resolve_b(b) as usize)
         }
-        1005 => {
-            let a = memory[pc + 1] as usize;
-            let b = memory[pc + 2] as usize;
-            Instruction::Jtr(memory[a], b)
-        }
-        1106 => {
+        6 => {
             let a = memory[pc + 1];
-            let b = memory[pc + 2] as usize;
-            Instruction::Jfl(a, b)
-        }
-        1006 => {
-            let a = memory[pc + 1] as usize;
-            let b = memory[pc + 2] as usize;
-            Instruction::Jfl(memory[a], b)
-        }
-        105 => {
-            let a = memory[pc + 1];
-            let b = memory[pc + 2] as usize;
-            Instruction::Jtr(a, memory[b] as usize)
-        }
-        106 => {
-            let a = memory[pc + 1];
-            let b = memory[pc + 2] as usize;
-            Instruction::Jfl(a, memory[b] as usize)
+            let b = memory[pc + 2];
+            Instruction::Jz(resolve_a(a), resolve_b(b) as usize)
         }
         7 => {
-            let a = memory[pc + 1] as usize;
-            let b = memory[pc + 2] as usize;
-            let out = memory[pc + 3] as usize;
-            Instruction::Lt(memory[a], memory[b], out)
-        }
-        108 => {
-            let a = memory[pc + 1];
-            let b = memory[pc + 2] as usize;
-            let out = memory[pc + 3] as usize;
-            Instruction::Eq(a, memory[b], out)
-        }
-        1108 => {
             let a = memory[pc + 1];
             let b = memory[pc + 2];
             let out = memory[pc + 3] as usize;
-            Instruction::Eq(a, b, out)
-        }
-        107 => {
-            let a = memory[pc + 1];
-            let b = memory[pc + 2] as usize;
-            let out = memory[pc + 3] as usize;
-            Instruction::Lt(a, memory[b], out)
-        }
-        1107 => {
-            let a = memory[pc + 1];
-            let b = memory[pc + 2];
-            let out = memory[pc + 3] as usize;
-            Instruction::Lt(a, b, out)
+            Instruction::Lt(resolve_a(a), resolve_b(b), out)
         }
         8 => {
-            let a = memory[pc + 1] as usize;
-            let b = memory[pc + 2] as usize;
-            let out = memory[pc + 3] as usize;
-            Instruction::Eq(memory[a], memory[b], out)
-        }
-        1007 => {
-            let a = memory[pc + 1] as usize;
+            let a = memory[pc + 1];
             let b = memory[pc + 2];
             let out = memory[pc + 3] as usize;
-            Instruction::Lt(memory[a], b, out)
+            Instruction::Eq(resolve_a(a), resolve_b(b), out)
         }
-        1008 => {
-            let a = memory[pc + 1] as usize;
-            let b = memory[pc + 2];
-            let out = memory[pc + 3] as usize;
-            Instruction::Eq(memory[a], b, out)
-        }
+        99 => Instruction::Hlt,
         code => panic!("unknown opcode {}", code)
     }
 }
 
-fn calc(starting_memory: &[isize]) {
+
+fn run(starting_memory: &[isize], fixinput: isize) -> Vec<isize> {
     let mut pc = 0;
+    let mut output = Vec::new();
     let mut memory: Vec<isize> = starting_memory.to_vec();
     loop {
         use Instruction::*;
         let opcode = opcode_decode(pc, &memory);
         match opcode {
-            Add(a, b, out) => {
-                memory[out] = a + b;
-                pc += 4;
-            }
-            Mul(a, b, out) => {
-                memory[out] = a * b;
-                pc += 4;
-            }
-            Inp(out) => {
-                memory[out] = 5; // part 2
-                pc += 2;
-            }
-            Out(a) => {
-                println!("OUT: {}", a);
-                pc += 2;
-            }
-            Jtr(a, b) => {
-                if a != 0 {
-                    pc = b;
-                } else {
-                    pc += 3;
-                }
-            }
-            Jfl(a, b) => {
-                if a == 0 {
-                    pc = b
-                } else {
-                    pc += 3;
-                }
-            }
-            Lt(a, b, out) => {
-                if a < b {
-                    memory[out] = 1
-                } else {
-                    memory[out] = 0
-                }
-                pc += 4;
-            }
-            Eq(a, b, out) => {
-                if a == b {
-                    memory[out] = 1
-                } else {
-                    memory[out] = 0
-                }
-                pc += 4;
-            }
-            Hlt => {
-                break;
-            }
+            Add(a, b, out) => { memory[out] = a + b; pc += 4; }
+            Mul(a, b, out) => { memory[out] = a * b; pc += 4; }
+            Inp(out) => { memory[out] = fixinput; pc += 2; }
+            Out(a) => { output.push(a); pc += 2; }
+            Jnz(a, b) => pc = if a != 0 { b } else { pc + 3 },
+            Jz(a, b) => pc = if a == 0 { b } else { pc + 3 },
+            Lt(a, b, out) => { memory[out] = if a < b { 1 } else { 0 }; pc += 4 }
+            Eq(a, b, out) => { memory[out] = if a == b { 1 } else { 0 }; pc += 4; }
+            Hlt => break
         }
     }
+    output
 }
 
 fn main() {
     let stdin = io::stdin();
     let input = stdin.lock().lines().next().unwrap().unwrap().split(',').map(|n| n.parse().unwrap()).collect::<Vec<isize>>();
 
-    calc(&input);
+    let part1 = run(&input, 1);
+    println!("Part 1: {}", part1.last().unwrap());
 
+    let part2 = run(&input, 5);
+    println!("Part 2: {}", part2.last().unwrap());
 }
