b13cfc1e3f08633bb1ac7e73c4ccd82b9265c047
János Illés <ijanos@gmail.com>
2019-12-09 23:36:26 +0100

    gotta go fast

--- rust/day09/src/main.rs
+++ rust/day09/src/main.rs
@@ -3,30 +3,32 @@ use std::io::prelude::*;
 use std::collections::VecDeque;
 use std::iter::FromIterator;
 
+type int = i64;
+
 #[derive(Debug)]
 enum Instruction {
-    Add(i128, i128, usize),
-    Eq(i128, i128, usize),
+    Add(int, int, usize),
+    Eq(int, int, usize),
     Hlt,
     Inp(usize),
-    Jnz(i128, usize),
-    Jz(i128, usize),
-    Lt(i128, i128, usize),
-    Rbo(i128),
-    Mul(i128, i128, usize),
-    Out(i128)
+    Jnz(int, usize),
+    Jz(int, usize),
+    Lt(int, int, usize),
+    Mul(int, int, usize),
+    Out(int)
+    Rbo(int),
 }
 
 #[derive(Clone)]
 struct IntCodeVM {
     pc: usize,
-    mem: Vec<i128>,
-    inputs: VecDeque<i128>,
-    relative_base: i128
+    mem: Vec<int>,
+    inputs: VecDeque<int>,
+    relative_base: int
 }
 
 impl IntCodeVM {
-    fn new(starting_memory: &[i128], inputbuffer: &[i128]) -> IntCodeVM {
+    fn new(starting_memory: &[int], inputbuffer: &[int]) -> IntCodeVM {
         let mut mem = starting_memory.to_vec();
         mem.resize(mem.len() + 4096, 0); // YOLO
         IntCodeVM {
@@ -37,7 +39,7 @@ impl IntCodeVM {
         }
     }
 
-    fn next_output(&mut self) -> Option<i128> {
+    fn next_output(&mut self) -> Option<int> {
         use Instruction::*;
         loop {
             match self.current_opcode_decode() {
@@ -55,7 +57,7 @@ impl IntCodeVM {
         }
     }
 
-    fn collect_output(&mut self) -> Vec<i128> {
+    fn collect_output(&mut self) -> Vec<int> {
         let mut out = Vec::new();
         while let Some(n) = self.next_output() {
             out.push(n)
@@ -63,14 +65,15 @@ impl IntCodeVM {
         out
     }
 
-    fn feed_input(&mut self, i: i128) {
+    fn feed_input(&mut self, i: int) {
         self.inputs.push_front(i);
     }
 
+    #[inline(always)]
     fn current_opcode_decode(&self) -> Instruction {
         let param = |n: usize| {
             let p = self.mem[self.pc + n];
-            match self.mem[self.pc] / (10 * 10_i128.pow(n as u32)) % 10 {
+            match self.mem[self.pc] / (10 * 10_u32.pow(n as u32) as int) % 10 {
                 1 => p,
                 2 => self.mem[(self.relative_base + p) as usize],
                 _ => self.mem[p as usize]
@@ -79,7 +82,7 @@ impl IntCodeVM {
 
         let dest = |n: usize| {
             let p = self.mem[self.pc + n];
-            match self.mem[self.pc] / (10 * 10_i128.pow(n as u32)) % 10 {
+            match self.mem[self.pc] / (10 * 10_u32.pow(n as u32) as int) % 10 {
                 1 => panic!("immidiate output paramter"),
                 2 => (self.relative_base + p) as usize,
                 _ => p as usize
@@ -103,19 +106,19 @@ impl IntCodeVM {
     }
 }
 
-fn part1(starting_memory: &[i128]) -> i128 {
+fn part1(starting_memory: &[int]) -> int {
     let mut vm = IntCodeVM::new(&starting_memory, &[1]);
     *vm.collect_output().last().unwrap()
 }
 
-fn part2(starting_memory: &[i128]) -> i128 {
+fn part2(starting_memory: &[int]) -> int {
     let mut vm = IntCodeVM::new(&starting_memory, &[2]);
     *vm.collect_output().last().unwrap()
 }
 
 fn main() {
     let stdin = io::stdin();
-    let input = stdin.lock().lines().next().unwrap().unwrap().split(',').map(|n| n.parse().unwrap()).collect::<Vec<i128>>();
+    let input = stdin.lock().lines().next().unwrap().unwrap().split(',').map(|n| n.parse().unwrap()).collect::<Vec<int>>();
 
     println!("Part 1: {}", part1(&input));
     println!("Part 2: {}", part2(&input));
@@ -128,14 +131,14 @@ mod tests {
     #[test]
     fn test_p1_example1() {
         let testinput = "104,1125899906842624,99";
-        let testinput = testinput.split(',').map(|n| n.parse().unwrap()).collect::<Vec<i128>>();
+        let testinput = testinput.split(',').map(|n| n.parse().unwrap()).collect::<Vec<int>>();
         let mut vm = IntCodeVM::new(&testinput, &[]);
         assert_eq!(vm.next_output(), Some(1125899906842624));
     }
     #[test]
     fn test_p1_example2() {
         let testinput = "109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99";
-        let testinput = testinput.split(',').map(|n| n.parse().unwrap()).collect::<Vec<i128>>();
+        let testinput = testinput.split(',').map(|n| n.parse().unwrap()).collect::<Vec<int>>();
         let mut vm = IntCodeVM::new(&testinput, &[]);
         assert_eq!(vm.collect_output(), testinput);
     }
